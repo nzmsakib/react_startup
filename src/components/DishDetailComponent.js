76f31ce34c3338c2ba79b21b09b9e67f8c90dcdb
Md Nazmus Sakib <60852942+nzmsakib@users.noreply.github.com>
2021-01-15 14:59:52 +0600

    Presentational and Container Component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -11,45 +11,47 @@ class DishDetail extends Component {
     }
 
     renderDish(dish) {
-        if (dish == null) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+
+    render() {
+        
+        if (this.props.dish == null) {
             return (
                 <div></div>
             );
         } else {
+            const comment = this.props.dish.comments.map((cmnt) => {
+                return (
+                    <div key={cmnt.id} className="mb-2">
+                        <p>{cmnt.comment}</p>
+                        <p>-- {cmnt.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmnt.date)))}</p>
+                    </div>
+                );
+            });
+    
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5">
+                            <h2>Comments</h2>
+                            {comment}
+                        </div>
+                    </div>
+                </div>
             );
         }
-    }
-
-    render() {
-
-        const comment = this.props.selectedDish.comments.map((cmnt) => {
-            return (
-                <div key={cmnt.id} className="mb-2">
-                    <p>{cmnt.comment}</p>
-                    <p>-- {cmnt.author}, {cmnt.date}</p>
-                </div>
-            );
-        })
-
-        return (
-            <div className="row">
-                <div className="col-12 col-md-5">
-                    {this.renderDish(this.props.selectedDish)}
-                </div>
-                <div className="col-12 col-md-5">
-                    <h2>Comments</h2>
-                    {comment}
-                </div>
-            </div>
-        );
     }
 }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        }
 
         console.log('Menu Component constructor is invoked');
     }
@@ -17,16 +12,12 @@ class Menu extends Component {
         console.log('Menu Component componentDidMount is invoked');
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
-
     render() {
 
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -42,7 +33,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}                
                 </div>
-                <DishDetail selectedDish={this.state.selectedDish} />
             </div>
         );
 
