1c93f6a0d55d7869ff78eb24f592927bb2470eb2
nzmsakib <60852942+nzmsakib@users.noreply.github.com>
2021-02-03 12:55:23 +0600

    react animation-components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,32 +4,39 @@ import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderDish({dish}) {
     return (
-        <Card>
-            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+            <Card>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
  function RenderComments({comments, postComment, dishId}) {
     const comment = comments.map((cmnt) => {
         return (
-            <div key={cmnt.id} className="mb-2">
-                <p>{cmnt.comment}</p>
-                <p>-- {cmnt.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmnt.date)))}</p>
-            </div>
+            <Fade in>
+                <div key={cmnt.id} className="mb-2">
+                    <p>{cmnt.comment}</p>
+                    <p>-- {cmnt.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmnt.date)))}</p>
+                </div>
+            </Fade>
         );
     });
 
     return (
         <div>
-            {comment}
+            <Stagger in>{comment}</Stagger>
             <CommentForm dishId={dishId} postComment={postComment} />
         </div>
     );
